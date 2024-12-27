bc6bf75080ef988732e54f21ca2a077c4838fb00
rabdiego <diegorabelodesa@gmail.com>
2024-12-27 15:46:17 -0300

    classe de vetores pronta

--- src/a2/test.py
+++ src/a2/test.py
@@ -1,5 +1,8 @@
 from vector import Vector
 
-v = Vector([1, 2 ,3])
-v2 = Vector([2, 3, 4])
-print(-v2)
+v = Vector([1, 2])
+v2 = Vector([2, 3]).norm()
+print(v2)
+print(v * v2)
+print(v2 * 1.6)
+print(v.slide(v2))
--- src/a2/vector.py
+++ src/a2/vector.py
@@ -17,7 +17,28 @@ class Vector:
         return str(self.data)
 
 
+    def get_mag(self) -> float:
+        """
+        Método auxiliar para pegar ||v||
+        """
+        s = 0
+        for element in self.data:
+            s += element
+        return s
+
+
+    def norm(self):
+        """
+        Método auxiliar para normalizar um vetor
+        """
+        return self/self.get_mag()
+
+
+
     def __add__(self, v2):
+        """
+        Adição de 2 vetores
+        """
         v3 = Vector([0 for _ in range(self.N)])  # Vetor "placeholder"
 
         for idx in range(self.N):
@@ -27,6 +48,9 @@ class Vector:
 
 
     def __sub__(self, v2):
+        """
+        Subtração de 2 vetores
+        """
         v3 = Vector([0 for _ in range(self.N)])  # Vetor "placeholder"
 
         for idx in range(self.N):
@@ -36,6 +60,9 @@ class Vector:
     
 
     def __neg__(self):
+        """
+        Negação de um vetor
+        """
         v2 = Vector([0 for _ in range(self.N)])  # Vetor "placeholder"
 
         for idx, element in enumerate(self.data):
@@ -44,7 +71,41 @@ class Vector:
         return v2
 
 
+    def __mul__(self, v2):
+        """
+        Multiplicação de um vetor por:
+        (1) um escalar, ou
+        (2) produto escalar,
+
+        dependendo do tipo do que é passado.
+
+        Não é implementado nesse operator overload o produto vetorial, pois
+        este possui propriedades anticomutativas que "bugariam" usando o __mul__ do
+        Python
+        """
+        if type(v2) == int or type(v2) == float:
+            return self._scalar_multiplication(v2)
+        
+        return self._dot_product(v2)
+    
+
+    def __truediv__(self, scalar: float):
+        """
+        Método auxiliar para dividir um vetor por um escalar
+        """
+
+        v2 = Vector([0 for _ in range(self.N)])  # Vetor "placeholder"
+
+        for idx, element in enumerate(self.data):
+            v2[idx] = element / scalar
+        
+        return v2
+
+
     def _scalar_multiplication(self, scalar: float):
+        """
+        Multiplicação por escalar
+        """
         v2 = Vector([0 for _ in range(self.N)])  # Vetor "placeholder"
 
         for idx, element in enumerate(self.data):
@@ -54,10 +115,62 @@ class Vector:
     
 
     def _dot_product(self, v2) -> float:
+        """
+        Produto escalar
+        """
         s = 0
 
         for idx in range(self.N):
             s += self[idx]*v2[idx]
         
         return s
+    
+
+    def cross(self, v2):
+        """
+        Produto vetorial
+
+        Utilizei a definição pela fórmula da determinante
+        """
+
+        v3 = Vector([0 for _ in range(3)])  # Placeholder
+
+        v3[0] = self[1]*v2[2] - self[2]*v2[1]
+        v3[1] = -(self[0]*v2[2] - self[2]*v2[0])
+        v3[2] = self[0]*v2[1] - self[1]*v2[0]
+
+        return v3
+    
+
+    def project(self, v2):
+        """
+        Projeção de um vetor em outro
+
+        No caso, usar v1.project(v2) retornará
+        o vetor v1 projetado em v2.
+        """
+        aux1 = self * v2
+        aux2 = v2 * v2
+        return v2 * (aux1/aux2)
+    
+
+    def reflect(self, v2):
+        """
+        Reflexão
+
+        No caso, usar v1.reflect(v2) retornará
+        o vetor v3 que é a reflexão de v1 sobre a
+        "reta" v2
+        """
+
+        v2_norm = v2.norm()
+        return self - v2_norm*(2*(self * v2_norm))
+
+
+    def slide(self, v2):
+        """
+        Deslizamento sobre uma parede, assumindo v2 como vetor unitário
+        """
+
+        return self - v2*(self*v2)
 
